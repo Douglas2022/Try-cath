8a3052a59ad368616ce558f8b54e9c684f62137f
Douglas Nunes <paulodnunes2016@gmail.com>
2023-03-05 18:38:47 -0300

    Try e Catch

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int resultado = n1 / n2;
+                Console.WriteLine(resultado);
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine(" Division by zero is not allowed ");
+            }
+
+
+        }
+    }
+}
